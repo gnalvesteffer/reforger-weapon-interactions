88a97a73603eed41e333bd3a320690de65474501
ss-gnalvesteffer <xorberax@gmail.com>
2022-06-01 22:18:55 -0500

    make action name to reflect adjustment state

--- scripts/Game/XOR_AdjustSightAction.c
+++ scripts/Game/XOR_AdjustSightAction.c
@@ -1,27 +1,39 @@
-class XOR_SightFlipAction : SCR_BaseWeaponAction
+class XOR_AdjustSightAction : SCR_BaseWeaponAction
 {
 	private static const vector DEFAULT_ANGLE = Vector(0, 0, 0);
 	private static const vector ADJUSTED_ANGLE = Vector(0, Math.PI / 2, 0);
-	
+	private static const string DEFAULT_ACTION_NAME = "Adjust Rear Sight Aperture (Large)";
+	private static const string ADJUSTED_ACTION_NAME = "Adjust Rear Sight Aperture (Small)";
+
 	private TNodeId _sightBoneIndex;
 	private bool _isAdjusted;
-	
+
 	override void Init(IEntity pOwnerEntity, GenericComponent pManagerComponent)
 	{
 		super.Init(pOwnerEntity, pManagerComponent);
 		_sightBoneIndex = pOwnerEntity.GetBoneIndex("w_sight");
 	}
-	
+
+	override bool GetActionNameScript(out string outName)
+	{
+		outName = DEFAULT_ACTION_NAME;
+		if (_isAdjusted)
+		{
+			outName = ADJUSTED_ACTION_NAME;
+		}
+		return true;
+	}
+
 	override bool CanBeShownScript(IEntity user)
 	{
 		return true;
 	}
-	
+
 	override bool CanBePerformedScript(IEntity user)
 	{
 		return true;
 	}
-	
+
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
 		Print("SIGHT ADJUSTED");
